c1c9e695a172dd066ad858d6217f8c5baee57c6c
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-08-30 22:39:06 -0300

    MVCcomErro

---
+++ Marketplace.mvc/Models/ClienteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marketplace.mvc.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Documento { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+        public string Email { get; set; }   
+    }
+}
